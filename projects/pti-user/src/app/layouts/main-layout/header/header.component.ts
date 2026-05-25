e0366a738fa7e9e36c8f58186bdbd24c02aa83fd
mrbeanintheword1520 <tongtphung2705@gmail.com>
2026-05-25 13:27:56 +0700

    hoc bai git lan 1

--- projects/pti-user/src/app/layouts/main-layout/header/header.component.ts
+++ projects/pti-user/src/app/layouts/main-layout/header/header.component.ts
@@ -1,16 +1,19 @@
 import { Component, inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { CustomerService } from '../../../shared/customer.service';
 import { ConsultationModalService } from '../../../pages/consultation/consultation-modal.service';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [RouterModule],
+  imports: [CommonModule, RouterModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
   protected readonly consultation = inject(ConsultationModalService);
+  protected readonly customerService = inject(CustomerService);
 
   openConsultation(event: Event): void {
     event.preventDefault();
--- projects/pti-user/src/app/layouts/main-layout/main-layout.component.ts
+++ projects/pti-user/src/app/layouts/main-layout/main-layout.component.ts
@@ -2,12 +2,13 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { Footer } from './footer/footer.component';
+import { WelcomePopupComponent } from '../../pages/welcome-popup/welcome-popup.component';
 import { QuickConsultationModalComponent } from '../../pages/consultation/quick-consultation-modal/quick-consultation-modal.component';
 
 @Component({
   selector: 'app-main-layout',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, Footer, QuickConsultationModalComponent],
+  imports: [RouterOutlet, HeaderComponent, Footer, WelcomePopupComponent, QuickConsultationModalComponent],
   templateUrl: './main-layout.component.html',
   styleUrl: './main-layout.component.scss'
 })
