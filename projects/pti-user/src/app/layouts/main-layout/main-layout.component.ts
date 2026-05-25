b285e17027fa50a12ca491aff6a80d161045b40f
mrbeanintheword1520 <tongtphung2705@gmail.com>
2026-05-25 13:48:05 +0700

    Chỉnh pop up

--- projects/pti-user/src/app/layouts/main-layout/main-layout.component.ts
+++ projects/pti-user/src/app/layouts/main-layout/main-layout.component.ts
@@ -2,14 +2,13 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { Footer } from './footer/footer.component';
-import { WelcomePopupComponent } from '../../pages/welcome-popup/welcome-popup.component';
 import { QuickConsultationModalComponent } from '../../pages/consultation/quick-consultation-modal/quick-consultation-modal.component';
 import { ProjectLeadPopupComponent } from '../../pages/lead-capture/project-lead-popup/project-lead-popup.component';
 
 @Component({
   selector: 'app-main-layout',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, Footer, WelcomePopupComponent, QuickConsultationModalComponent, ProjectLeadPopupComponent],
+  imports: [RouterOutlet, HeaderComponent, Footer, QuickConsultationModalComponent, ProjectLeadPopupComponent],
   templateUrl: './main-layout.component.html',
   styleUrl: './main-layout.component.scss'
 })
